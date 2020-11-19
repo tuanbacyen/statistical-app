c7e34daded74edb94a8f8f41c81c8ab8a8618b32
PAT <tuanbacyen@gmail.com>
2020-11-19 18:02:04 +0700

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,29 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 const User = mongoose.model('User');
+const bcrypt = require('bcrypt');
 
-const user_login = (req, res) => {
-  res.render('user/login', {
-    flash: "XXXXX"
-  });
-  // GmsApp.find().sort({ _id: -1 }).lean().exec((error, docs) => {
-  //   if (error) {
-  //     console.log('list error: ', error);
-  //     return;
-  //   }
-
-  //   res.render('gms_app/index', {
-  //     list: docs
-  //   });
-  // });
+const user_login = async (req, res) => {
+  const user = await User.findOne({ username: 'admin' });
+  if (user === null) {
+    console.log('create admin');
+    var u = new User();
+    u.username = 'admin';
+    u.password = await bcrypt.hash('123456', 10);
+    u.save();
+  }
+  res.render('user/login');
 }
+
+const user_sign_in = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  console.log(req.body);
+  console.log(hashedPassword);
+  res.redirect('/login')
+}
+
+
 
 module.exports = {
   user_login,
+  user_sign_in,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 // => router user
 router.get('/login', userController.user_login);
+router.post('/sign_in', userController.user_sign_in);
 
 module.exports = router;
